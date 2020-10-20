79f5cedf087f27432884b76917180b72bbc5ca53
Carlos Alfonso Gomez Hernandez <caragomezher@unal.edu.co>
2020-10-20 14:30:55 -0500

    Graph with bokeh

--- 3.Stats/ramdom_path.py
+++ 3.Stats/ramdom_path.py
@@ -2,6 +2,8 @@ from borr import BorrachoTradicional
 from field import Field
 from coord import Coord
 
+from bokeh.plotting import figure, show
+
 
 def caminata(field, borr, steps):
     start = field.get_coords(borr)
@@ -25,17 +27,33 @@ def test_walked(steps, repetitions, type_borr):
     return distances
 
 
+def draw(axis_x, axis_y):
+    graf = figure(title='Camino aleatorio', x_axis_label='pasos',
+                  y_axis_label='distancia')
+    arrx = axis_x
+    arry = axis_y
+    print(arrx)
+    print(arry)
+    graf.line(x=arrx, y=arry)
+    # graf..
+    show(graf)
+
+
 def main(distances_walked, repetitions, tipo):
+    distances_per_walk = []
 
     for distance in distances_walked:
         distances = test_walked(distance, repetitions, tipo)
         media = round(sum(distances) / len(distances), 4)
         distances_max = max(distances)
         distances_min = min(distances)
+        distances_per_walk.append(int(media))
         print(f'{tipo.__name__} caminara aleatoria de {distance} pasos')
         print(f'Media = {media}')
         print(f'Max = {distances_max}')
         print(f'Min = {distances_min}')
+
+    draw(distances_walked, distances_per_walk)
 
 
 if __name__ == "__main__":
